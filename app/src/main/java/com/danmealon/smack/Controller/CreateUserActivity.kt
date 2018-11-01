395ef6f588baa5dd1774425728f22408b36619f6
Daniiar2204 <daniiar2204@gmail.com>
2018-11-01 16:58:05 -0700

    broadcast manager

--- app/src/main/java/com/danmealon/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/danmealon/smack/Controller/CreateUserActivity.kt
@@ -1,12 +1,17 @@
 package com.danmealon.smack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
+
 import com.danmealon.smack.R
 import com.danmealon.smack.Services.AuthService
 import com.danmealon.smack.Services.UserDataService
+import com.danmealon.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +23,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view:View){
@@ -56,28 +62,67 @@ class CreateUserActivity : AppCompatActivity() {
 
     fun createUserClicked(view:View){
 
+        enableSpinner(true)
         val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()//password is an id of username field
 
-        AuthService.registerUser(this, email,password){registerSuccess->
-            if (registerSuccess){
-                AuthService.loginUser(this, email, password) {loginSuccess->
-                    if(loginSuccess){
-                        AuthService.createUser(this,userName,email,userAvatar, avatarColor) {createSuccess->
-                            if (createSuccess){
-                                println(UserDataService.avatarName)
-                                println(UserDataService.avatarColor)
-                                println(UserDataService.name)
-                                finish()
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+
+            AuthService.registerUser(this, email,password){registerSuccess->
+                if (registerSuccess){
+                    AuthService.loginUser(this, email, password) {loginSuccess->
+                        if(loginSuccess){
+                            AuthService.createUser(this,userName,email,userAvatar, avatarColor) {createSuccess->
+                                if (createSuccess){
+
+                                    //implementing broadcast by using the intent of type action and string
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+
+                                    enableSpinner(false)
+                                    finish()
+                                } else{
+                                    errorToast()
+                                }
                             }
+                        }else{
+                            errorToast()
                         }
                     }
+
+                }else{
+                    errorToast()
                 }
 
             }
-
+        } else { //if one of them is empty
+            Toast.makeText(this,"Make sure user's name, email and password are filled in",
+                Toast.LENGTH_LONG).show()
+            enableSpinner(false)
         }
 
     }
+
+    fun errorToast() {
+        Toast.makeText(this,"Something went wrong, please try again!",
+            Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            createSpinner.visibility = View.VISIBLE //we want ot show it
+
+        }else{
+            createSpinner.visibility = View.INVISIBLE
+        }
+        // we want to disable create user button, avatar image picker and color generator
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
+
+    }
+
 }
