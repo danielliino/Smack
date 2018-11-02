de238ec535e3f1882f0a0605d8ffe2042735ddff
Daniiar2204 <daniiar2204@gmail.com>
2018-11-02 05:06:40 -0700

    lesson 16

---
+++ app/src/main/java/com/danmealon/smack/Controller/App.kt
@@ -0,0 +1,4 @@
+package com.danmealon.smack.Controller
+
+class App {
+}
--- app/src/main/java/com/danmealon/smack/Utilities/SharedPrefs.kt
+++ app/src/main/java/com/danmealon/smack/Utilities/SharedPrefs.kt
@@ -1,4 +1,28 @@
 package com.danmealon.smack.Utilities
 
-class SharedPrefs {
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPrefs(context: Context) {
+    val PREFS_FILENAME = "prefs"
+    val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0) //0 means content is private
+
+    //creating constants for keys
+    val IS_LOGGED_IN = "isLoggedIn"
+    val AUTH_TOKEN = "authToken"
+    val USER_EMAIL = "userEmail"
+
+    //creating custom getters and setters
+    var isLoggedIn: Boolean
+        //if value is set we going to get it, if no we provide default variable (false)
+    get() = prefs.getBoolean(IS_LOGGED_IN, false)
+    set(value) = prefs.edit().putBoolean(IS_LOGGED_IN, value).apply()
+
+    var authToken: String
+    get() = prefs.getString(AUTH_TOKEN,"")
+    set(value) = prefs.edit().putString(AUTH_TOKEN,value).apply()
+
+    var userEmail: String
+    get() = prefs.getString(USER_EMAIL,"")
+    set(value) = prefs.edit().putString(USER_EMAIL,value).apply()
 }
