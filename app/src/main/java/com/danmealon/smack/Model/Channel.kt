7726c8cc4d98f4ebbae597cd6deed9bcda313c93
Daniiar2204 <daniiar2204@gmail.com>
2018-11-02 02:28:46 -0700

    lesson 13

--- app/src/main/java/com/danmealon/smack/Model/Channel.kt
+++ app/src/main/java/com/danmealon/smack/Model/Channel.kt
@@ -1,4 +1,8 @@
 package com.danmealon.smack.Model
 
-class Channel {
+class Channel (val name: String, val description: String, val id: String) {
+    //creating listView
+    override fun toString(): String {
+        return "#$name" //it is a modern standard that channels have # on front
+    }
 }
---
+++ app/src/main/java/com/danmealon/smack/Services/MessageService.kt
@@ -0,0 +1,4 @@
+package com.danmealon.smack.Services
+
+object MessageService {
+}
